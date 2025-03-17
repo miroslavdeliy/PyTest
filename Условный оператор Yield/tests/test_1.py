014d14ea4410fe873683679767a839e079414caf
Deliy Miroslav <miroslavdely@gmail.com>
2025-03-17 16:55:33 +0300

    2-я версия

--- Условный оператор Yield/tests/test_1.py
+++ Условный оператор Yield/tests/test_1.py
@@ -1,16 +1,19 @@
-# Импортирование фреймворка
 import pytest
 
-# Фиктура
+# Фикстура возвращает список
 @pytest.fixture
-def set_up():
-   print("Сообщение перед началом теста")
-   yield
-   print('Сообщение после выполнения теста')
+def data():
+    test_list = [1, 2, 3, 4, 5]
+    print(f'Тестовый список {test_list}')
+    yield test_list
+    print('Расчет выполнен')
 
-# 2 псевдотеста с использованием фиктуры
-def test_sending_message_1(set_up):
-   print("Сообщение теста 1")
 
-def test_sending_message_2(set_up):
-   print("Соощение теста 2")
+# Расчет суммы списка из фикстуры
+def test_sum(data):
+    print(f'Сумма списка: {sum(data)}')
+
+
+# Расчет среднего списка из фикстуры
+def test_average(data):
+    print(f'Среднее списка: {sum(data)/len(data)}')
